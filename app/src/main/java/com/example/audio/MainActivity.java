61d7230a2bd0a05daffcc396cd7311a019eed875
Nevin-Thinagar <nevin.thinagar@gmail.com>
2023-03-24 22:50:04 -0400

    Fixed audio permission issue

--- app/src/main/java/com/example/audio/MainActivity.java
+++ app/src/main/java/com/example/audio/MainActivity.java
@@ -9,6 +9,7 @@ import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
+import android.Manifest;
 
 import java.io.File;
 import java.io.IOException;
@@ -37,12 +38,10 @@ public class MainActivity extends AppCompatActivity {
             // Log.e(LOG_TAG, "prepare() failed");
             throw new RuntimeException(e);
         }
-
-
     }
 
     private void requestAudioPermissions() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.) != PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
 
             if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.RECORD_AUDIO)) {
                 Toast.makeText(this, "Please grant permissions to record audio", Toast.LENGTH_LONG).show();
