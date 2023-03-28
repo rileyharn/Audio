a65b16bc82be82a2ac94765ba33842bf9b659936
rileyharn <riley@harn.me>
2023-03-28 13:09:23 -0400

    I added a new page for the profile.

--- app/src/main/java/com/example/audio/userProfile.java
+++ app/src/main/java/com/example/audio/userProfile.java
@@ -3,12 +3,22 @@ package com.example.audio;
 import static androidx.core.content.ContextCompat.startActivity;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class userProfile {
-    public void openRecordingPage(View v){
-        startActivity(new Intent(user_profile.this, MainActivity.class));
-    }
+import androidx.appcompat.app.AppCompatActivity;
 
-}
+public class userProfile extends AppCompatActivity {
+    private Button recordNowButton_profilePage;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_profile);
+
+    }
+        public void openActivity2(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        }
+    }
--- app/src/main/java/com/example/audio/user_profile.java
+++
@@ -1,7 +0,0 @@
-package com.example.audio;
-
-import android.os.Bundle;
-
-public interface user_profile {
-    void onCreate(Bundle savedInstanceState);
-}
