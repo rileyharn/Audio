82a4b695dfe2ebad262ad0731f6c31c449e0d07a
natha <nathannlam05@gmail.com>
2023-04-25 14:10:37 -0400

    message

--- app/src/main/java/com/example/audio/LoginScreen.java
+++ app/src/main/java/com/example/audio/LoginScreen.java
@@ -63,7 +63,7 @@ public class LoginScreen extends AppCompatActivity {
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
-        signInLauncher.launch(presignInIntent);
+        signInLauncher.launch(AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers).build());
 
     }
 
@@ -90,18 +90,18 @@ public class LoginScreen extends AppCompatActivity {
         TextView tv1 = (TextView)findViewById(R.id.currentUserText);
 
     }
-//    @Override
-//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//
-//        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
-//        if (requestCode == RC_SIGN_IN) {
-//            // The Task returned from this call is always completed, no need to attach
-//            // a listener.
-//            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-//            handleSignInResult(task);
-//        }
-//    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
     private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
         try {
             account = completedTask.getResult(ApiException.class);
