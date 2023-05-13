8aaaa34bec6a7a88e34b9118a742a4d012259233
Nevin-Thinagar <nevin.thinagar@gmail.com>
2023-05-12 21:38:31 -0400

    Added randomized audio playing in database page

--- app/src/main/java/com/example/audio/User_Profile.java
+++ app/src/main/java/com/example/audio/User_Profile.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.File;
+
 public class User_Profile extends AppCompatActivity {
 
 
@@ -34,6 +36,9 @@ public class User_Profile extends AppCompatActivity {
         recordNowXML = findViewById(R.id.recordNowButton);
         createFamilyGroupXML = findViewById(R.id.createAFamilyGroupButton);
         signOutXML = findViewById(R.id.signOutButton);
+
+        ((MyApplication) this.getApplication()).setCurDir(new File(getExternalFilesDir(null), ((MyApplication) this.getApplication()).getUserName()));
+
         familyGroupsXML.setOnClickListener(new View.OnClickListener() {
 
             @Override
